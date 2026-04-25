5af4419ed0a02a9c5f493174473a0f28850bf307
Bhavik Patel <41283211+Bhavik-15@users.noreply.github.com>
2026-04-25 21:51:13 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,104 +1,11 @@
 // Chapter 1 — Thol Diaries
-var tholPhotos = [
-  "IMG_4401.JPG",
-  "IMG_4402.JPG",
-  "IMG_4403.JPG",
-  "IMG_4404.JPG",
-  "IMG_4405.JPG",
-  "img_4456.jpeg",
-  "img_4448.jpeg",
-  "img_4446.jpeg",
-  "img_4206.jpeg"
-];
+var tholPhotos = [  ];
 
 // Chapter 2 — Engagement Diaries
-var engagementPhotos = [
-  "engagement (1).JPG", "engagement (3).JPG", "engagement (4).JPG", "engagement (5).JPG",
-  "engagement (6).JPG", "engagement (7).JPG", "engagement (8).JPG", "engagement (9).JPG",
-  "engagement (10).JPG", "engagement (11).JPG", "engagement (12).JPG", "engagement (13).JPG",
-  "engagement (14).JPG", "engagement (15).JPG", "engagement (16).JPG", "engagement (17).JPG",
-  "engagement (18).JPG", "engagement (19).JPG", "engagement (20).JPG", "engagement (21).JPG",
-  "engagement (22).JPG", "engagement (23).JPG", "engagement (24).JPG", "engagement (25).JPG",
-  "engagement (26).JPG", "engagement (27).JPG", "engagement (28).JPG", "engagement (29).JPG",
-  "engagement (30).JPG", "engagement (31).JPG", "engagement (32).JPG", "engagement (33).JPG",
-  "engagement (34).JPG", "engagement (35).JPG", "engagement (36).JPG", "engagement (37).JPG",
-  "engagement (38).JPG", "engagement (39).JPG", "engagement (40).JPG", "engagement (41).JPG",
-  "engagement (42).JPG", "engagement (43).JPG", "engagement (44).JPG", "engagement (45).JPG",
-  "engagement (46).JPG", "engagement (47).JPG", "engagement (48).JPG", "engagement (49).JPG",
-  "engagement (50).JPG", "engagement (51).JPG", "engagement (52).JPG", "engagement (53).JPG",
-  "engagement (54).JPG", "engagement (55).JPG", "engagement (56).JPG", "engagement (57).JPG",
-  "engagement (58).JPG", "engagement (59).JPG", "engagement (60).JPG", "engagement (61).JPG",
-  "engagement (62).JPG", "engagement (63).JPG", "engagement (64).JPG", "engagement (65).JPG",
-  "engagement (66).JPG", "engagement (67).JPG", "engagement (68).JPG", "engagement (69).JPG",
-  "engagement (70).JPG", "engagement (71).JPG", "engagement (72).JPG", "engagement (73).JPG",
-  "engagement (74).JPG", "engagement (75).JPG", "engagement (76).JPG", "engagement (77).JPG",
-  "engagement (78).JPG", "engagement (79).JPG", "engagement (80).JPG", "engagement (81).JPG",
-  "engagement (82).JPG", "engagement (83).JPG", "engagement (84).JPG", "engagement (85).JPG",
-  "engagement (86).JPG", "engagement (87).JPG", "engagement (88).JPG", "engagement (89).JPG",
-  "engagement (90).JPG", "engagement (91).JPG", "engagement (92).JPG", "engagement (93).JPG",
-  "engagement (94).JPG", "engagement (95).JPG", "engagement (96).JPG", "engagement (97).JPG",
-  "engagement (98).JPG", "engagement (99).JPG", "engagement (100).JPG", "engagement (101).JPG",
-  "engagement (102).JPG", "engagement (103).JPG", "engagement (104).JPG", "engagement (105).JPG",
-  "engagement (106).JPG", "engagement (107).JPG", "engagement (108).JPG", "engagement (109).JPG",
-  "engagement (110).JPG", "engagement (111).JPG", "engagement (112).JPG", "engagement (113).JPG",
-  "engagement (114).JPG", "engagement (115).JPG", "engagement (116).JPG", "engagement (10000).JPG"
-];
+var engagementPhotos = [  ];
 
 // Chapter 3 — Dwarka Diaries
-var dwarkaPhotos = [
-  "img_4398.jpg",
-  "img_6748.jpeg",
-  "img_6749.jpeg",
-  "img_6750.jpeg",
-  "img_6751.jpeg",
-  "img_6752.jpeg",
-  "img_6753.jpeg",
-  "img_6756.jpeg",
-  "img_6757.jpeg",
-  "img_6758.jpeg",
-  "img_6759.jpeg",
-  "img_6815.jpeg",
-  "img_6816.jpeg",
-  "img_6814.jpeg",
-  "img_6878.jpeg",
-  "img_6877.jpeg",
-  "img_6875.jpeg",
-  "img_6872.jpeg",
-  "img_6871.jpeg",
-  "img_6870.jpeg",
-  "img_6867.jpeg",
-  "img_6866.jpeg",
-  "img_6865.jpeg",
-  "img_6864.jpeg",
-  "img_6863.jpeg",
-  "img_6862.jpeg",
-  "img_6841.jpeg",
-  "img_6839.jpeg",
-  "img_6835.jpeg",
-  "img_6833.jpeg",
-  "img_6830.jpeg",
-  "img_6829.jpeg",
-  "img_6828.jpeg",
-  "img_6826.jpeg",
-  "img_6761.jpeg"
-];
+var dwarkaPhotos = [ ];
 
 // Chapter 4 — Kanku Pagla
-var kankuPhotos = [
-  "kanku.jpg", "kanku_(5).jpg", "kanku_(7).jpg", "kanku_(9).jpg", "kanku_(11).jpg",
-  "kanku_(12).jpg", "kanku_(13).jpg", "kanku_(14).jpg", "kanku_(16).jpg", "kanku_(17).jpg",
-  "kanku_(18).jpg", "kanku_(19).jpg", "kanku_(20).jpg", "kanku_(21).jpg", "kanku_(22).jpg",
-  "kanku_(24).jpg", "kanku_(25).jpg", "kanku_(26).jpg", "kanku_(29).jpg", "kanku_(30).jpg",
-  "kanku_(31).jpg", "kanku_(32).jpg", "kanku_(33).jpg", "kanku_(34).jpg", "kanku_(35).jpg",
-  "kanku_(36).jpg", "kanku_(37).jpg", "kanku_(38).jpg", "kanku_(39).jpg", "kanku_(40).jpg",
-  "kanku_(41).jpg", "kanku_(42).jpg", "kanku_(51).jpg", "kanku_(52).jpg", "kanku_(53).jpg",
-  "kanku_(54).jpg", "kanku_(55).jpg", "kanku_(56).jpg", "kanku_(57).jpg", "kanku_(58).jpg",
-  "kanku_(59).jpg", "kanku_(61).jpg", "kanku_(62).jpg", "kanku_(63).jpg", "kanku_(64).jpg",
-  "kanku_(65).jpg", "kanku_(66).jpg", "kanku_(67).jpg", "kanku_(68).jpg", "kanku_(69).jpg",
-  "kanku_(70).jpg", "kanku_(71).jpg", "kanku_(72).jpg", "kanku_(73).jpg", "kanku_(74).jpg",
-  "kanku_(75).jpg", "kanku_(76).jpg", "kanku_(77).jpg", "kanku_(78).jpg", "kanku_(79).jpg",
-  "kanku_(81).jpg", "kanku_(82).jpg", "kanku_(83).jpg", "kanku_(84).jpg", "kanku_(85).jpg",
-  "kanku_(86).jpg", "kanku_(87).jpg", "kanku_(88).jpg", "kanku_(89).jpg", "kanku_(90).jpg",
-  "kanku_(91).jpg", "kanku_(92).jpg", "kanku_(93).jpg", "kanku_(94).jpg", "kanku_(95).jpg",
-  "kanku_(96).jpg"
-];
+var kankuPhotos = [    ];
