724ed32b7bd01ad64de1d971664a7015f9248219
Bhavik Patel <41283211+Bhavik-15@users.noreply.github.com>
2026-04-25 21:57:41 +0530

    Update data.js photo list

--- data.js
+++ data.js
@@ -5,7 +5,14 @@ var tholPhotos = [  ];
 var engagementPhotos = [  ];
 
 // Chapter 3 — Dwarka Diaries
-var dwarkaPhotos = [ ];
+var dwarkaPhotos = [
+  "img_6753.jpeg",
+  "img_6752.jpeg",
+  "img_6751.jpeg",
+  "img_6748.jpeg",
+  "img_6749.jpeg",
+  "img_6750.jpeg"
+];
 
 // Chapter 4 — Kanku Pagla
 var kankuPhotos = [    ];
