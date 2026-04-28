605970f894349286331babee438cd2147822f7e7
Bhavik Patel <41283211+Bhavik-15@users.noreply.github.com>
2026-04-28 18:36:25 +0530

    Update data.js photo list

--- data.js
+++ data.js
@@ -5,7 +5,11 @@ var tholPhotos = [
 
 // Chapter 2 — Engagement Diaries
 var engagementPhotos = [
-  "b0bddf0a-7018-4ca5-a15f-077b00d2482a.jpeg"
+  "b0bddf0a-7018-4ca5-a15f-077b00d2482a.jpeg",
+  "5696ed44-ff57-4a4b-98d8-b1e3ed99804e.jpeg",
+  "e357d555-ba70-45e6-b859-958a02a90dab.jpeg",
+  "ca40c8f8-f525-47b0-b9ef-bcefa6435965.jpeg",
+  "7873709c-f859-4933-828b-890571a9d6b3.jpeg"
 ];
 
 // Chapter 3 — Dwarka Diaries
